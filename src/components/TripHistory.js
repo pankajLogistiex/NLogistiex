255e3407445eda61bcd9fe77486fcbde72895b29
khushboo-mudgal <khushboo.mudgal@logistiex.com>
2023-05-31 11:16:28 +0530

    fixed

--- src/components/TripHistory.js
+++ src/components/TripHistory.js
@@ -47,7 +47,6 @@ export default function TripHistory({navigation, route}) {
   
   const focus = useIsFocused();
 
-console.log(userId)
 useEffect(() => {
     const fetchData = async () => {
       try {
